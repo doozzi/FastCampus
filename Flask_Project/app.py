de2d718e7091f394128870adaae3016f7e4ce785
doozzi <gustn2607@gmail.com>
2023-02-01 21:29:09 +0900

    #

--- Flask_Project/app.py
+++ Flask_Project/app.py
@@ -2,7 +2,7 @@ import math
 import datetime
 from flask_pymongo import PyMongo
 from flask import Flask, render_template, request, redirect
-
+#
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/local"
 mongo = PyMongo(app)
